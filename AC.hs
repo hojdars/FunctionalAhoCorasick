d3eea13de452f78e385e1d93ed4b766685ef66b9
Ashen <ashencz@gmail.com>
2016-05-14 17:54:29 +0200

    commented, ready for release

--- AC.hs
+++ AC.hs
@@ -6,16 +6,23 @@ data Config = Config (DAState, [(Char,DAState)]) deriving(Show)
 data BackEdge = BackEdge (DAState,DAState) deriving(Show)
 data ShortEdge = ShortEdge (DAState,DAState) deriving(Show)
 
+instance Eq Config where Config (a,b) == Config (c,d) = (a==c) && (b==d)
 instance Eq DAState where
     Lambda  == Lambda   = True
     State a == State b  = a == b
     _       == _        = False
 
-instance Eq Config where
-    Config (a,b) == Config (c,d) = (a==c) && (b==d)
-
 convert :: Maybe a -> a
 convert (Just a) = a
+
+convertLambda :: Maybe DAState -> DAState
+convertLambda Nothing = Lambda
+convertLambda (Just a) = a
+
+getStr :: DAState -> String
+getStr st | st == Lambda = ""
+          | otherwise = str
+          where State str = st
 
 mySubstring :: String -> Int -> Int -> String
 mySubstring sez begin end = drop begin (take end sez)
@@ -25,12 +32,22 @@ mySubstring sez begin end = drop begin (take end sez)
 erase :: DAState -> [Config] -> [Config]
 erase state cfgs = filter (\ (Config a) -> (fst a)/=state) cfgs
 
--- prelude's lookup for Config type
-lookfor :: DAState -> [Config] -> Maybe Config
+lookfor :: DAState -> [Config] -> Maybe Config -- prelude's lookup for Config type
 lookfor state [] = Nothing
-lookfor state (c:confs) = if state == cstate then Just c
-                        else lookfor state confs
+lookfor state (c:confs) = if state == cstate then Just c else lookfor state confs
                     where Config (cstate,nic) = c
+
+lookforBack :: DAState -> [BackEdge] -> Maybe DAState -- prelude's lookup for BackEdge type
+lookforBack state [] = Nothing
+lookforBack state (b:backs) = if state == firstState then Just nextState
+                            else lookforBack state backs
+    where BackEdge (firstState,nextState) = b
+
+lookForShort :: DAState -> [ShortEdge] -> Maybe DAState -- prelude's lookup for ShortEdge type
+lookForShort state [] = Nothing
+lookForShort state (b:backs) = if state == firstState then Just nextState
+                                else lookForShort state backs
+    where ShortEdge (firstState,nextState) = b
 
 -- generates one forward edge
 -- it looks up the config in config list for the state we want to create, if it already exists (like in barb + barc test, bar already exists)
@@ -45,16 +62,12 @@ gen w c i = if result == Nothing then ( Config( (State (mySubstring w 0 i)), [ (
                 where   result = lookfor (State (mySubstring w 0 i)) c
                         Config (first_state,second_cond) = convert result
 
--- generates states and forward edges for one word
-genForWord :: String -> [Config] -> Int -> [Config]
-genForWord word conf num | num == ( (length word)) = conf
-                         | otherwise = genForWord word (gen word conf num) (num+1)
-
 -- generates forward edges and all the states
 genWords :: [String] -> [Config] -> [Config]
 genWords [] conf = conf
 genWords (w:word) conf = genWords word (genForWord w conf 0)
-
+    where genForWord word conf num  | num == ( (length word)) = conf -- generates states and forward edges for one word
+                                    | otherwise = genForWord word (gen word conf num) (num+1)
 
 -- takes one forward step from given state over given letter, need to provide Configurations
 forwardStep :: DAState -> Char -> [Config] -> DAState
@@ -67,13 +80,8 @@ forwardStep state letter confs = if result == Nothing then Lambda
         Config (first, transitions) = convert result
         edge = lookup letter transitions
 
--- prelude's lookup for BackEdge type
-lookforBack :: DAState -> [BackEdge] -> Maybe DAState
-lookforBack state [] = Nothing
-lookforBack state (b:backs) = if state == firstState then Just nextState
-                        else lookforBack state backs
-                    where BackEdge (firstState,nextState) = b
-
+-- a function for the step back, it tries to go forward, if it can it does, else it jumps back once more and repeats
+-- either it lands in lambda or it succeeds trying
 stepBack :: DAState -> Char -> [Config] -> ([BackEdge],[ShortEdge]) -> DAState
 stepBack Lambda letter confs (backs,shorts) = forwardStep Lambda letter confs
 stepBack state letter confs (b,s) = if forwardEdge == Nothing then stepBack nextBack letter confs (b,s)
@@ -83,85 +91,69 @@ stepBack state letter confs (b,s) = if forwardEdge == Nothing then stepBack next
     where
         forwardEdge = lookfor state confs
         Config (inState, transitions) = convert forwardEdge
-        f Nothing = Lambda
-        f (Just a) = a
-        nextBack = f $ lookforBack state b
+        nextBack = convertLambda $ lookforBack state b
 
--- ACKrok from MJ's book
-acKrok :: DAState -> Char -> [Config] -> ([BackEdge],[ShortEdge]) -> DAState
-acKrok state letter confs (backs,shorts) =
-        if forwardEdge == Nothing then stepBack state letter confs (backs,shorts)
+-- one step of the automata, goes directly if it can else it backtracks
+acStep :: DAState -> Char -> [Config] -> ([BackEdge],[ShortEdge]) -> DAState
+acStep state letter confs (backs,shorts) =
+         if forwardEdge == Nothing then stepBack state letter confs (backs,shorts) -- if there exists at least one edge from the current node
         else
-            if forwardJump == Nothing then stepBack state letter confs (backs,shorts)
-            else (convert forwardJump)
+            if forwardJump == Nothing then stepBack state letter confs (backs,shorts) -- if there is no edge from this node for this letter, we backtrack
+            else (convert forwardJump) -- else we travel to this new node ~ state
     where
         forwardEdge = lookfor state confs
         Config (inState, transitions) = convert forwardEdge
         forwardJump = lookup letter transitions
 
-lookForShort :: DAState -> [ShortEdge] -> Maybe DAState
-lookForShort state [] = Nothing
-lookForShort state (b:backs) = if state == firstState then Just nextState
-                        else lookForShort state backs
-                    where ShortEdge (firstState,nextState) = b
-
-getStr :: DAState -> String
-getStr st | st == Lambda = ""
-          | otherwise = str
-          where
-            State str = st
-
+-- builds the backs and shorts from the given node
 forEachBuild :: DAState -> [(Char,DAState)] -> [Config] -> [String] -> ([BackEdge],[ShortEdge]) -> ([BackEdge],[ShortEdge])
 forEachBuild iState [] confs finalWords backShort = backShort
 forEachBuild iState ((isLetter,sState):rest) confs finalWords (backs,shorts) = forEachBuild iState rest confs finalWords ( (newBack:backs)  , (newShort:shorts))
     where
         zpetI = lookforBack iState backs
-        f Nothing = Lambda
-        f (Just a) = a
-        zState = (acKrok (f zpetI) isLetter confs (backs,shorts))
+        zState = (acStep ( convertLambda zpetI) isLetter confs (backs,shorts))
         newBack = BackEdge (  sState , zState )
-        zkratkaZ = f $ lookForShort zState shorts
+        zkratkaZ = convertLambda $ lookForShort zState shorts
         strZState = (getStr zState)
         newShort = if strZState == "" then ShortEdge ( sState ,  zkratkaZ )
-                    else
-                        if elem strZState finalWords then ShortEdge ( sState ,  zState   )
-                        else ShortEdge ( sState ,  zkratkaZ   )
+                    else if elem strZState finalWords then ShortEdge ( sState ,  zState   )
+                         else ShortEdge ( sState ,  zkratkaZ   )
 
-
+-- constructs the back and short edges
+-- if we reached the end we return our accumulator
+-- else we add the children of the current node to the queue and call forEachBuild to build the backs&shorts from this node
 workQ :: [Config] -> [Config] -> [String] -> ([BackEdge],[ShortEdge]) -> ([BackEdge],[ShortEdge])
 workQ [] confs finalWords (back,short) = (back,short)
 workQ (front:queue) confs finalWords (back,short) = workQ (queue ++ addToQ forwEdges confs) confs finalWords (forEachBuild state_i forwEdges confs finalWords (back,short))
     where
         Config (state_i,forwEdges) = front
-        addToQ [] confs = []
+        addToQ [] confs = [] -- we need to add to the queue all the children of the current node
         addToQ ((letter,state):edges) confs = if result == Nothing then (addToQ edges confs)
-                                      else ((convert result):(addToQ edges confs))
-            where
-                result = lookfor state confs
+                                              else ((convert result):(addToQ edges confs))
+            where result = lookfor state confs
 
--- gets a list of words, returns the list of final words, forwardConfigs, backedges and shortedges
+-- gets a list of words, returns the list of final words, forwardConfigs, backedges and shortedges, using all the functions listed above
 treeBuild :: [String] -> ( [String] ,[Config], ([BackEdge],[ShortEdge]) )
 treeBuild needles = (needles, confs , backShort )
     where
-        confs = (genWords needles [])
-        lambdaState = lookfor Lambda confs
+        confs = (genWords needles []) -- generate the forward edges and trie structure
+        lambdaState = lookfor Lambda confs -- we find Lambda -> the root
         Config (smt, children) = convert lambdaState
-        initConf = map (\ (letter, newState) -> convert ( lookfor newState confs ) ) $ children
-        backShort = workQ initConf confs needles ([],[])
+        initConf = map (\ (letter, newState) -> convert ( lookfor newState confs ) ) $ children -- initConf are initial entries in the queue -> children of Lambda
+        backShort = workQ initConf confs needles ([],[]) -- we construct the back and short edges
 
+-- function that goes through all the short jumps whenever we arrive to a node, checking all the possible occurences of some of the needles
 goThroughShorts :: DAState -> [ShortEdge] -> [String] -> [String] -> [String]
-goThroughShorts Lambda _ _ f = f
+goThroughShorts Lambda _ _ found = found
 goThroughShorts state shorts finalWords found = goThroughShorts nextState shorts finalWords newfound
     where
-        nextState = f $ lookForShort state shorts
-        f Nothing = Lambda
-        f (Just a) = a
-        newfound = if (elem (getStr state) finalWords) then (getStr state):found
-                        else found
+        nextState = convertLambda $ lookForShort state shorts
+        newfound = if (elem (getStr state) finalWords) then (getStr state):found else found
 
-acHledej :: DAState -> [String] -> String -> ( [String] ,[Config], ([BackEdge],[ShortEdge]) ) -> [String] -> ( DAState, [String] )
-acHledej state _ [] (finalWords, _, (backs,shorts)) found = (state, found ++ (goThroughShorts state shorts finalWords []))
-acHledej state (needles) (h:hay) (finalWords, confs, (backs,shorts)) found = acHledej (acKrok state h confs (backs,shorts)) needles hay (finalWords, confs, (backs,shorts)) ( found ++ (goThroughShorts state shorts finalWords []))
+-- we do acStep for the automata, each time checking all the shorts via goThroughShorts
+acSearch :: DAState -> [String] -> String -> ( [String] ,[Config], ([BackEdge],[ShortEdge]) ) -> [String] -> ( DAState, [String] )
+acSearch state _ [] (finalWords, _, (backs,shorts)) found = (state, found ++ (goThroughShorts state shorts finalWords []))
+acSearch state (needles) (h:hay) (finalWords, confs, (backs,shorts)) found = acSearch (acStep state h confs (backs,shorts)) needles hay (finalWords, confs, (backs,shorts)) ( found ++ (goThroughShorts state shorts finalWords []))
 
 ac :: [String] -> String -> [String]
-ac needles hay = snd $ acHledej Lambda needles hay (treeBuild needles) []
+ac needles hay = snd $ acSearch Lambda needles hay (treeBuild needles) []
